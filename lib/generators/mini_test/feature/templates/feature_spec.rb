0cd21c4386c4e03ca2b4a6ba16a574193956b61e
Mike Moore <mike@blowmage.com>
2013-01-11 10:08:40 -0700

    Update feature generator to use the Capybara spec DSL

--- lib/generators/mini_test/feature/templates/feature_spec.rb
+++ lib/generators/mini_test/feature/templates/feature_spec.rb
@@ -1,8 +1,8 @@
 require "minitest_helper"
 
 # To be handled correctly by Capybara this spec must end with "Feature Test"
-describe "<%= class_name %> Feature Test" do
-  it "sanity" do
+feature "<%= class_name %> Feature Test" do
+  scenario "the test is sound" do
     visit root_path
     page.must_have_content "Hello World"
     page.wont_have_content "Goobye All!"
